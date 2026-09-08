9a79a38963c85c374ca2c4383f870b0a47da5f53
Tech-Magic-Glitch <yanni.wohlleber@gmail.com>
2026-09-08 19:28:42 +0200

    fix

--- Only_fight.py
+++ Only_fight.py
@@ -4,11 +4,11 @@ Name = input("Enter your name: ")
 player = Player(Name)
 player.show()
 
-goblin = Goblin()
-orc = Orc()
-Dragon_Rider = Dragon_Rider()
-Dragon_Lord = Dragon_Lord()
-Dragon = Dragon()
+
+
+
+
+
 
 
 print(f"Welcome, {Name}! Welcome to only fight, a game where you can fight monsters and gain experience points to level up your character. Good luck on your journey! Type in 'quit' to exit the game at any time.")
@@ -22,15 +22,22 @@ print('5. Dragon Lord')
 while True:
  rival = input('Enter the number of your rival: ')
  if rival == '1':
+  goblin = Goblin()
   [Goblin]
   player.fight(goblin)
  if rival == '2':
+  orc = Orc()
   player.fight(orc)
+
  if rival == '3':
+  Dragon = Dragon()
   player.fight(Dragon)
  if rival == '4':
+  Dragon_Rider = Dragon_Rider()
   player.fight(Dragon_Rider)
+
  if rival == '5':
+  Dragon_Lord = Dragon_Lord()
   player.fight(Dragon_Lord)
  if rival == quit:
   print('Thanks for playing! Goodbye!')
